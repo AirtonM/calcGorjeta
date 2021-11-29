8bb4ef19f3aa3b700093db83e5862218df964e14
Airton Marinho <airtonmarinho15@gmail.com>
2021-11-29 08:45:49 -0300

    Vê ai Jhon

--- App.js
+++ App.js
@@ -1,21 +1,69 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, Button } from 'react-native';
+import styled from "styled-components/native";
 
-export default function App() {
+const Page = styled.SafeAreaView `
+  flex: 1;
+  align-items: center;
+`;
+
+const HeaderText = styled.Text`
+  font-size: 25px;
+  font-weight: bold;
+  margin: 10px 0;
+`;
+
+const Input = styled.TextInput`
+  width: 90%;
+  height: 50px;
+  font-size: 18px;
+  background-color: #EEE;
+  margin-top: 20px;
+  border-radius: 10px;
+  padding: 10px;
+`;
+
+const CalcButton = styled.Button`
+  margin-top: 20px;
+`;
+
+const ResultArea = styled.View`
+  width: 100%;
+  margin-top: 30px;
+  background-color: #EEE;
+  padding: 20px;
+  justify-content: center;
+  align-items: center;
+`;
+
+export default() => {
+
+  const [bill, setBill] = useState('');
+  const [tip, setTip] = useState(0);
+
+  const calc = () => {
+    let nBill = parseFloat(bill); 
+
+    if(nBill) {
+      setTrp ( (10/100) * nBill );
+    }else {
+      alert("Digite o valor da conta")
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Page>
+      <HeaderText>
+        Calculadora de Gorjeta
+      </HeaderText>
+      <Input 
+        placeholder = "Quanto deu a conta?"
+        placeholderTextColor = "#000"
+        keyboardType = "numeric"
+        value={bill}
+        onChangeText={n=>setBill(n)}
+      />
+      <CalcButton title="Calcular" onPress={calc} />
+    </Page>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
